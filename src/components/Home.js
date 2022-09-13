f4ea65368bdc92536592a9b533f79c406a114a7f
DhananjayThete <thetedhananjay97@gmail.com>
2022-09-14 02:02:21 +0530

    Five changes in Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { useState } from "react";
 
+import logo1 from '../images/animated1.gif';
+import logo2 from '../images/paint1.png';
+import logo3 from '../images/animated2.gif';
 
 const data = [
     {
@@ -29,7 +32,7 @@ export default function Home() {
     };
 
     return (
-        <div style={{height:'730px'}}>
+        <div className='home2' style={{height:'730px'}}>
 
             <div className='home' >
                 <h1 style={{ color: "green", fontFamily: "sans-serif" }}><b>Welcome To Online Agriculture  Market</b></h1>
@@ -48,8 +51,8 @@ export default function Home() {
 
                 </pre>
             </div>  
-            <div className="carous">
-            <Carousel activeIndex={index} onSelect={handleSelect}>
+            <div className="">
+            {/* <Carousel activeIndex={index} onSelect={handleSelect}>
        {data.map((slide, i) => {
         return (
           <Carousel.Item>        
@@ -66,7 +69,75 @@ export default function Home() {
         )
       })}
       
-    </Carousel>
+    </Carousel> */}
+    <div className='container'>
+      <div className='row'>
+    <div className='col-xl-4 logo1 '>
+      <div>
+      <img className="logoimg img-fluid" style={{height:'275px'}} src={logo1}></img>
+      </div>
+
+      <div>
+      <br/> <br/> 
+      <table class="table text-start ">
+               <thead>
+               <tr class="table-danger">
+                    <th ><h3><b>Features</b> </h3></th>
+                  </tr>
+                  <tr class="table-info">
+                    <th ><b>Open Farmer Login</b> </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="table-info">
+                    <td className='hometable'> Farmer can Add Crop</td>
+                  </tr>
+                  <tr class="table-info">
+                    <td>Farmer Can Select Maximum Bid</td>
+                  </tr>
+                  <tr class="table-info">
+                    <td >Finally Sell The Crop</td>
+                  </tr>
+                </tbody>
+              </table>
+      </div>
+            </div> 
+
+     <div className='col-xl-4 logo1 '><img className="img-fluid logo1" src={logo2}></img></div>
+
+    <div className='col-xl-4 logo1 '>
+      <div>
+      <img className="logoimg img-fluid" style={{height:'275px'}} src={logo3}></img>
+      </div>
+
+      <div>
+      <br/> <br/> 
+      <table class="table text-start ">
+               <thead>
+               <tr class="table-danger">
+                    <th ><h3><b>Features</b> </h3></th>
+                  </tr>
+                  <tr class="table-info">
+                    <th ><b>Open Vendor Login</b> </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="table-info">
+                    <td className='hometable'> Vendor Can Search Crops</td>
+                  </tr>
+                  <tr class="table-info">
+                    <td>Bid On that Crop</td>
+                  </tr>
+                  <tr class="table-info">
+                    <td >Can Buy the Crop Virtually</td>
+                  </tr>
+                </tbody>
+              </table>
+      </div>
+      
+      </div> 
+    </div>
+    </div>
             </div>            
             </div>
 
